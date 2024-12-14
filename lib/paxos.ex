d816a4c7889a466bc0f15c54b98d2b965b74afdf
mdawso <dawsonmathew08@gmail.com>
2024-12-14 00:18:11 +0000

    new code outline

--- lib/paxos.ex
+++ lib/paxos.ex
@@ -1,32 +1,19 @@
 defmodule Paxos do
 
-  # implement required api functions
-  # start(atom:name, atom[]:participants) -> pid: pid
-  # propose(pid: pid, inst: inst, float: value, float: t)
+    defmodule Paxos.Participant do
 
-  @spec start(atom, [atom]) :: pid
-  def start(name, participants) do
-    pid = spawn(fn -> paxos_process(name, participants) end)
-    :global.register_name(name, pid)
-    pid
-  end
+        defmodule Paxos.Participant.Proposer do
 
-  defp paxos_process(name, participants) do
-    # Implementation of the Paxos process goes here
-    receive do
-      # Handle messages
+        end
+
+        defmodule Paxos.Participant.Acceptor do
+
+        end
+
+        defmodule Paxos.Participant.Learner do
+
+        end
+
     end
-  end
-
-  @spec propose(pid, float, float, float) :: {:decision, float} | {:abort} | {:timeout}
-  def propose(pid, inst, value, t) do
-    send(pid, {:propose, self(), inst, value})
-    receive do
-      {:decision, v} -> {:decision, v}
-      {:abort} -> {:abort}
-    after
-      t -> {:timeout}
-    end
-  end
 
 end
