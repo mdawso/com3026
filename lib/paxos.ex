31da6bc754f5ac2d82938230fbba202beea7b363
mdawso <dawsonmathew08@gmail.com>
2024-12-14 01:30:54 +0000

    added proposer acceptor and learner spawn funcs

--- lib/paxos.ex
+++ lib/paxos.ex
@@ -1,16 +1,76 @@
 defmodule Paxos do
 
-    defmodule Paxos.Participant do
+    defmodule Participant do
 
-        defmodule Paxos.Participant.Proposer do
+        defmodule Proposer do
+
+            def start(name) do
+                pid = spawn(Proposer, :init, [name])
+                case :global.re_register_name(name, pid) do
+                    :yes ->
+                        IO.puts("Registered proposer #{name}")
+                        {:ok, pid}
+                    :no ->
+                        {:error, :registration_failed}
+                end
+            end
+
+            def init(name) do
+                IO.puts("Init function called!")
+                run()
+            end
+
+            def run() do
+                IO.puts("Run function called!")
+            end
 
         end
 
-        defmodule Paxos.Participant.Acceptor do
+        defmodule Acceptor do
+
+            def start(name) do
+                pid = spawn(Acceptor, :init, [name])
+                case :global.re_register_name(name, pid) do
+                    :yes ->
+                        IO.puts("Registered proposer #{name}")
+                        {:ok, pid}
+                    :no ->
+                        {:error, :registration_failed}
+                end
+            end
+
+            def init(name) do
+                IO.puts("Init function called!")
+                run()
+            end
+
+            def run() do
+                IO.puts("Run function called!")
+            end
 
         end
 
-        defmodule Paxos.Participant.Learner do
+        defmodule Learner do
+
+            def start(name) do
+                pid = spawn(Learner, :init, [name])
+                case :global.re_register_name(name, pid) do
+                    :yes ->
+                        IO.puts("Registered proposer #{name}")
+                        {:ok, pid}
+                    :no ->
+                        {:error, :registration_failed}
+                end
+            end
+
+            def init(name) do
+                IO.puts("Init function called!")
+                run()
+            end
+
+            def run() do
+                IO.puts("Run function called!")
+            end
 
         end
 
