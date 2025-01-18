63f607a0be9d690b11c12bfed368ba03ae600b92
mdawso <dawsonmathew08@gmail.com>
2025-01-18 05:36:44 +0000

    server impl basic

--- my_test_script.exs
+++ my_test_script.exs
@@ -1,16 +1,15 @@
+# Tests the paxos impl with my server
+
 IEx.Helpers.c("paxos.ex", ".")
-IEx.Helpers.c("account_server.ex", ".")
+IEx.Helpers.c("server.ex", ".")
 
 p1pid = Paxos.start(:p1, [:p1,:p2,:p3])
 p2pid = Paxos.start(:p2, [:p1,:p2,:p3])
 p3pid = Paxos.start(:p3, [:p1,:p2,:p3])
 
-accs = AccountServer.start(:accs, :p1)
+serverpid = Server.start(:server, :p1, 100)
 
-AccountServer.deposit(accs, 69)
-bal = AccountServer.balance(accs)
-IO.puts(bal)
-
-AccountServer.withdraw(accs, 42)
-bal = AccountServer.balance(accs)
-IO.puts(bal)
+out = Server.book_seat(:server, 10) |> IO.inspect(label: "Booking seat 10")
+out = Server.book_seat(:server, 10) |> IO.inspect(label: "Booking seat 10 again")
+out = Server.book_seat(:server, 0) |> IO.inspect(label: "Booking seat 0")
+out = Server.book_seat(:server, 101) |> IO.inspect(label: "Booking seat 101")
---
+++ my_test_script_acs.exs
@@ -0,0 +1,18 @@
+# Tests the paxos impl with account_server
+
+IEx.Helpers.c("paxos.ex", ".")
+IEx.Helpers.c("account_server.ex", ".")
+
+p1pid = Paxos.start(:p1, [:p1,:p2,:p3])
+p2pid = Paxos.start(:p2, [:p1,:p2,:p3])
+p3pid = Paxos.start(:p3, [:p1,:p2,:p3])
+
+accs = AccountServer.start(:accs, :p1)
+
+AccountServer.deposit(accs, 69)
+bal = AccountServer.balance(accs)
+IO.puts(bal)
+
+AccountServer.withdraw(accs, 42)
+bal = AccountServer.balance(accs)
+IO.puts(bal)
