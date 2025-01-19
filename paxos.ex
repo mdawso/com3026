98c6c425da691d324779b13ff219db8dd8fe15a1
mdawso <dawsonmathew08@gmail.com>
2025-01-19 23:06:36 +0000

    code cleanup and logging

--- paxos.ex
+++ paxos.ex
@@ -1,8 +1,11 @@
 defmodule Paxos do
 
+	defp log(x) do if true do IO.puts(x) end end
+
 	def start(name, participants) do
 		pid = spawn(Paxos, :init, [name, participants])
 		:global.re_register_name(name, pid)
+		log("Started Paxos process with name #{name} and participants #{inspect(participants)}")
 		pid
 	end
 
@@ -42,9 +45,11 @@ defmodule Paxos do
 	end
 
 	defp handle_check_instance(state, pid, inst, value) do
+		log("Handle Check Instance")
 		state = %{state | parent_name: pid, parent_pid: pid}
 		case Map.get(state.decisions_map, inst) do
 			nil ->
+				log("No decision found for instance #{inst}, broadcasting proposal")
 				beb(state.participants, {:update_instances_map, inst, pid, [value]})
 				if state.inst == nil do
 					send(self(), {:broadcast, pid, inst, value})
@@ -53,30 +58,37 @@ defmodule Paxos do
 					state
 				end
 			decision ->
+				log("Decision found for instance #{inst}, sending decision")
 				send(state.parent_name, {:decision, decision})
 				state
 		end
 	end
 
 	defp handle_update_instance_map(state, inst_id, parent_pid, proposals) do
+		log("Handle Update Instance Map")
 		case Map.has_key?(state.instances, inst_id) do
 			false ->
+				log("Instance #{inst_id} not found, updating instances map")
 				new_instances = Map.put(state.instances, inst_id, {parent_pid, proposals})
 				%{state | instances: new_instances}
 			true ->
+				log("Instance #{inst_id} already exists")
 				state
 		end
 	end
 
 	defp handle_return_decision(state, pid, inst) do
+		log("Handle Return Decision")
 		inst_decision = Map.get(state.decisions_map, inst)
 		send(pid, {:return_decision, inst_decision})
 		state
 	end
 
 	defp handle_broadcast_proposal(state, value, inst) do
+		log("Handle Broadcast Proposal")
 		state = %{state | proposal_h: value, inst: inst}
 		if state.name == state.leader && !state.prepare_called do
+			log("Leader broadcasting prepare message")
 			beb(state.participants, {:prepare, increment_ballot_number(state.bal, state.name), state.name, state.inst})
 			%{state | prepare_called: true}
 		else
@@ -85,10 +97,12 @@ defmodule Paxos do
 	end
 
 	defp handle_broadcast(state, pid, inst, value) do
+		log("Handle Broadcast")
 		state = %{state | proposal: value, inst: inst}
 		beb(state.participants, {:broadcast_proposal, value, inst})
-		if(state.name == state.leader && state.prepare_called != true) do
-			beb(state.participants, {:prepare, increment_ballot_number(state.bal, state.name),state.name, state.inst })
+		if state.name == state.leader && !state.prepare_called do
+			log("Leader broadcasting prepare message")
+			beb(state.participants, {:prepare, increment_ballot_number(state.bal, state.name), state.name, state.inst})
 			state = %{state | prepare_called: true}
 			state
 		else
@@ -97,9 +111,11 @@ defmodule Paxos do
 	end
 
 	defp handle_leader_elect(state, p) do
-		if(state.name == p) do
-			if(state.proposal != nil || state.proposal_h != nil) do
-				beb(state.participants, {:prepare, increment_ballot_number(state.bal, p),p, state.inst })
+		log("Handle Leader Elect")
+		if state.name == p do
+			log("This node is the new leader")
+			if state.proposal != nil || state.proposal_h != nil do
+				beb(state.participants, {:prepare, increment_ballot_number(state.bal, p), p, state.inst})
 				state
 			end
 		end
@@ -107,11 +123,14 @@ defmodule Paxos do
 	end
 
 	defp handle_prepare(state, b, leader, inst) do
+		log("Handle Prepare")
 		if state.inst == inst do
 			if compare_ballot(b, &>/2, state.bal) do
+				log("Ballot is higher, sending prepared message")
 				unicast(leader, {:prepared, b, state.a_bal, state.a_val, state.inst})
 				%{state | bal: b, leader: leader, a_bal_max: nil, a_val_max: nil}
 			else
+				log("Ballot is lower, sending nack message")
 				unicast(leader, {:nack, b, inst})
 				state
 			end
@@ -122,6 +141,7 @@ defmodule Paxos do
 	end
 
 	defp handle_prepared(state, b, a_bal, a_val, inst) do
+		log("Handle Prepared")
 		if inst == state.inst do
 			state = %{state | prepared_quorum: state.prepared_quorum + 1}
 
@@ -137,6 +157,7 @@ defmodule Paxos do
 				end
 
 			if state.leader == state.name and state.prepared_quorum >= (floor(length(state.participants) / 2) + 1) do
+				log("Quorum reached, sending accept message")
 				state = %{state | prepared_quorum: 0}
 
 				v = 
@@ -157,11 +178,14 @@ defmodule Paxos do
 	end
 
 	defp handle_accept(state, b, v, sender, inst) do
+		log("Handle Accept")
 		if inst == state.inst do
 			if compare_ballot(b, &>=/2, state.bal) do
+				log("Ballot is higher or equal, sending accepted message")
 				unicast(sender, {:accepted, b, inst})
 				%{state | bal: b, a_val: v, a_bal: b}
 			else
+				log("Ballot is lower, sending nack message")
 				unicast(sender, {:nack, b, inst})
 				state
 			end
@@ -171,11 +195,13 @@ defmodule Paxos do
 	end
 
 	defp handle_accepted(state, b, inst) do
+		log("Handle Accepted")
 		if inst == state.inst do
-			state = %{state | accepted_quorum: state.accepted_quorum+1}
-			state = if(state.leader == state.name) do
-				if(state.accepted_quorum >= (floor(length(state.participants)/2) +1)) do # check if there is a quorum of accepted
-					state=%{state | accepted_quorum: 0}
+			state = %{state | accepted_quorum: state.accepted_quorum + 1}
+			state = if state.leader == state.name do
+				if state.accepted_quorum >= (floor(length(state.participants) / 2) + 1) do
+					log("Quorum reached, broadcasting decision")
+					state = %{state | accepted_quorum: 0}
 					if state.parent_name != nil do
 						send(state.parent_name, {:decision, state.a_val})
 					end
@@ -194,19 +220,22 @@ defmodule Paxos do
 	end
 
 	defp handle_nack(state, b, inst) do
-		if state.inst == inst  do
+		log("Handle Nack")
+		if state.inst == inst do
 			if state.leader == state.name do
+				log("Leader received nack, aborting")
 				if state.parent_pid != nil do
 					send(state.parent_pid, {:abort})
 					beb(state.participants, {:abort, b, inst})
 				end
-					beb(state.participants, {:abort, b, inst})
+				beb(state.participants, {:abort, b, inst})
 			end
 		end
 		state
 	end
 
 	defp handle_abort(state, b, inst) do
+		log("Handle Abort")
 		if state.inst == inst do
 			if state.parent_pid != nil do
 				send(state.parent_pid, {:abort})
@@ -217,7 +246,8 @@ defmodule Paxos do
 	end
 
 	defp handle_recieved_decision(state, v, inst) do
-		state= %{state | decision: v, decisions_map: Map.put(state.decisions_map, inst, v), instances: Map.delete(state.instances, inst), inst: nil}
+		log("Handle Received Decision")
+		state = %{state | decision: v, decisions_map: Map.put(state.decisions_map, inst, v), instances: Map.delete(state.instances, inst), inst: nil}
 		state = %{state |
 			bal: {0, state.name},
 			a_bal: nil,
@@ -234,7 +264,7 @@ defmodule Paxos do
 		}
 		foundInstance = Enum.at(Map.keys(state.instances), 0)
 		foundInstanceProposal = Map.get(state.instances, foundInstance)
-		if(foundInstance != nil) do
+		if foundInstance != nil do
 			{pid, proposals} = foundInstanceProposal
 			if state.name == state.leader do
 				send(self(), {:broadcast, pid, foundInstance, Enum.at(proposals, 0)})
@@ -266,10 +296,8 @@ defmodule Paxos do
 		Process.send_after(self(), {:timeout}, t)
 		send(pid, {:check_instance, self(), inst, value})
 		result = receive do
-			{:decision, v} ->
-				{:decision,v}
-			{:abort} ->
-				{:abort}
+			{:decision, v} -> {:decision,v}
+			{:abort} -> {:abort}
 		after
 			t -> {:timeout}
 		end
@@ -278,8 +306,7 @@ defmodule Paxos do
 	def get_decision(pid, inst, t) do
 		send(pid, {:return_decision, self(), inst})
 		result = receive do
-			{:return_decision, v } ->
-					v
+			{:return_decision, v } -> v
 		after
 			t -> nil
 		end
@@ -295,30 +322,23 @@ defmodule Paxos do
 
 	defp beb(dest, m), do: for p <- dest, do: unicast(p, m)
 
-	defp register_name(name, pid, link \\ true) do
-		case :global.re_register_name(name, pid) do
-			:yes ->
-				if link do
-					Process.link(pid)
-				end
-				pid
-			:no ->
-				Process.exit(pid, :kill)
-				:error
-		end
+	defp lexicographical_compare(a, b) do
+		if a == b do 0 end
+		if a>b do 1 end
+		if a<b do -1 end
 	end
 
-	defp compare_ballot({bal1, pid1}, operator, {bal2, pid2}) do
-		case bal1 - bal2 do
-			0 -> operator.(String.compare(Atom.to_string(pid1), Atom.to_string(pid2)), 0)
-			diff -> operator.(diff, 0)
-		end
-	end
+	defp compare_ballot(left, operator, right) do
+    	operator.(ballot_compare(left, right),0)
+  	end
+
+  	defp ballot_compare(a, b) do
+    	diff = elem(a, 0) - elem(b, 0)
+    	if diff == 0, do: lexicographical_compare(elem(a, 1), elem(b, 1)), else: diff end
 
 	defp increment_ballot_number(ballot_tuple, leader) do
-		{elem(ballot_tuple,0)+1, leader}
-
-	end
+  		{elem(ballot_tuple,0)+1, leader}
+  	end
 end
 
 defmodule LeaderDetector do
@@ -399,15 +419,5 @@ defmodule LeaderDetector do
 				:error
 		end
 	end
-
-	defp compare_ballot({bal1, pid1}, operator, {bal2, pid2}) do
-		case bal1 - bal2 do
-			0 -> operator.(String.compare(Atom.to_string(pid1), Atom.to_string(pid2)), 0)
-			diff -> operator.(diff, 0)
-		end
-	end
-
-	defp increment_ballot_number(ballot_tuple, leader) do
-		{elem(ballot_tuple,0)+1, leader}
-	end
+	
 end
